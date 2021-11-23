68091cd90f57d4817cff36d02f73b1885b94751d
이진우 <leejinwoo1126@gmail.com>
2021-11-23 15:01:54 +0900

    Update #3 ApiAuthController 내용 추가
    @Deprecated 표시하여 사용하지 않는 클래스 내용

--- src/main/java/com/uno/getinline/controller/api/ApiAuthController.java
+++ src/main/java/com/uno/getinline/controller/api/ApiAuthController.java
@@ -1,4 +1,24 @@
 package com.uno.getinline.controller.api;
 
+import com.uno.getinline.dto.AdminRequest;
+import com.uno.getinline.dto.ApiDataResponse;
+import com.uno.getinline.dto.LoginRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Deprecated
+//@RequestMapping("/api")
+//@RestController
 public class ApiAuthController {
+
+    @PostMapping("/sign-up")
+    public ApiDataResponse<String> signUp(@RequestBody AdminRequest adminRequest){
+        return ApiDataResponse.empty();
+    }
+
+    @PostMapping("/login")
+    public ApiDataResponse<String> login(@RequestBody LoginRequest loginRequest){
+        return ApiDataResponse.empty();
+    }
+
 }
