4080fce3a9dac2153907179f59e9a3479ff95f99
이진우 <leejinwoo1126@gmail.com>
2021-11-23 22:41:59 +0900

    Update #3 ConstraintViolationException 처리 Handler 추가
    Validation 시 발생하는 ConstraintViolationException 처리 내용 추가

--- src/main/java/com/uno/getinline/error/APIExceptionHandler.java
+++ src/main/java/com/uno/getinline/error/APIExceptionHandler.java
@@ -3,6 +3,7 @@ package com.uno.getinline.error;
 import com.uno.getinline.constant.ErrorCode;
 import com.uno.getinline.dto.ApiErrorResponse;
 import com.uno.getinline.exception.GeneralException;
+import org.hibernate.validator.internal.util.annotation.ConstraintAnnotationDescriptor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +13,34 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolationException;
+
 @RestControllerAdvice(annotations = RestController.class)
 public class APIExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /**
+     * ch03-02 validation 테스트 중 추가, ConstraintViolationException 수동 처리 필요
+     * @param e
+     * @param request
+     * @return
+     */
+    @ExceptionHandler
+    public ResponseEntity<Object> general(ConstraintViolationException e, WebRequest request){
+        ErrorCode errorCode = ErrorCode.VALIDATION_ERROR;
+        HttpStatus status = errorCode.isClientSideError() ?
+                HttpStatus.BAD_REQUEST :
+                HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return super.handleExceptionInternal(
+                e,
+                ApiErrorResponse.of(false,errorCode.getCode(), errorCode.getMessage(e) ),
+                HttpHeaders.EMPTY,
+                status,
+                request
+        );
+    }
+
+
     /**
      * ResponseEntityExceptionHandler 상속 받으면 스프링에서 관리하는 exception 처리 가능(하지만 깔끔하지 않음)
      * protected 되어 있는 ResponseEntity<Object> handleExceptionInternal
