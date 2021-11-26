7c38074c24b0a724ef66965b59d3d9d31ba28f0c
이진우 <leejinwoo1126@gmail.com>
2021-11-26 22:46:12 +0900

    Update #5 EventDTO 수정에 따른 테스트 데이터 내용 임시 수정
    EventDTO 수정에 따른 테스트 데이터 내용 임시 수정

--- src/test/java/com/uno/getinline/controller/api/ApiEventControllerTest.java
+++ src/test/java/com/uno/getinline/controller/api/ApiEventControllerTest.java
@@ -183,7 +183,9 @@ class ApiEventControllerTest {
 
 
     private EventDTO createEventDTO(){
-        return  EventDTO.of(1L,
+        return  EventDTO.of(
+                1L,
+                1L,
                 "오후 운동",
                 EventStatus.OPENED,
                 LocalDateTime.of(2021, 1, 1, 13, 0, 0),
